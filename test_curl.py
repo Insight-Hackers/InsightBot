47bb608c810e44f1c39de80f95d31248865c3a31
yaelshneor2004 <yashneor@g.jct.ac.il>
2025-06-12 00:10:50 +0300

    list by alya

--- test_curl.py
+++ test_curl.py
@@ -3,22 +3,20 @@ import pandas as pd
 import io
 
 
-url = 'https://files.slack.com/files-pri/T08TR1VA5JS-F0910FJJRU2/download/list'
-api_token = 'xoxb-8943063345638-9002480214405-Vbh7kHjdgcLrfYoQKq34kJO9'
+url = 'https://files.slack.com/files-pri/T08TR1VA5JS-F091DFRQ9U1/download/list'
+api_token = 'xoxb-8943063345638-9002480214405-SsIbcsHZTsw4LorLdRbeYZ4d'
 headers = {
     'Authorization': f'Bearer {api_token}',
     'Content-Type': 'application/json'
 }
 res = requests.get(url, headers=headers)
-
-csv_url = res.json()['files'][0]['list_csv_download_url']
-headers = {
-    "Authorization": f"Bearer {api_token}"
-}
-response = requests.get(csv_url, headers=headers)
-response.raise_for_status() 
-df = pd.read_csv(io.StringIO(response.text))
-
-
+csv_url = res.json()['list_csv_download_url']
+# Download the CSV file
+csv_res = requests.get(url=csv_url,headers=headers)
+csv_res.raise_for_status() 
+csv_data = csv_res.content.decode('utf-8').splitlines()
+total_csv = [dict(zip(csv_data[0].split(','), line.split(','))) for line in csv_data[1:]]
+from pprint import pprint
+pprint(total_csv)
 
 
