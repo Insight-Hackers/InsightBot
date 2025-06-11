02e0215811f3d5939338536889b47393f90d8807
yaelshneor2004 <yashneor@g.jct.ac.il>
2025-06-11 22:09:01 +0300

    list

--- test_curl.py
+++ test_curl.py
@@ -1,4 +1,7 @@
 import requests
+import pandas as pd
+import io
+
 
 url = 'https://files.slack.com/files-pri/T08TR1VA5JS-F0910FJJRU2/download/list'
 api_token = 'xoxb-8943063345638-9002480214405-Vbh7kHjdgcLrfYoQKq34kJO9'
@@ -7,5 +10,12 @@ headers = {
     'Content-Type': 'application/json'
 }
 res = requests.get(url, headers=headers)
-print(res.status_code)
-print(res.json())
+if res.status_code == 200:
+    df = pd.read_csv(io.StringIO(res.text))
+    print("Status code:", res.status_code)
+    print("DataFrame:")
+    print(df)
+else:
+    print("Failed to download list.")
+    print("Status code:", res.status_code)
+    print("Response text:", res.text)
