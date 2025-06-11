66d0efd364b06f30712e745d5bbd1019a528f18b
yaelshneor2004 <yashneor@g.jct.ac.il>
2025-06-11 22:52:36 +0300

    list and fix

--- test_curl.py
+++ test_curl.py
@@ -2,23 +2,16 @@ import requests
 import pandas as pd
 import io
 
-
-url = 'https://files.slack.com/files-pri/T08TR1VA5JS-F0910FJJRU2/download/list'
+csv_url = "https://files.slack.com/files-pri/T08TR1VA5JS-F091DFRQ9U1/csv/list"
 api_token = 'xoxb-8943063345638-9002480214405-Vbh7kHjdgcLrfYoQKq34kJO9'
-headers = {
-    'Authorization': f'Bearer {api_token}',
-    'Content-Type': 'application/json'
-}
-res = requests.get(url, headers=headers)
 
-csv_url = res.json()['files'][0]['list_csv_download_url']
 headers = {
     "Authorization": f"Bearer {api_token}"
 }
+
 response = requests.get(csv_url, headers=headers)
-response.raise_for_status() 
+response.raise_for_status()
+
 df = pd.read_csv(io.StringIO(response.text))
 
-
-
-
+print(df)
